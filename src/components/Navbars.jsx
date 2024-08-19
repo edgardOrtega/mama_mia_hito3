488d4a32d71e9d393d3d33db619c238302a62d77
EdgardOrtegaPino <edgard.ortega.p@gmail.com>
2024-08-18 23:09:02 -0400

    agregando cambios al navbar

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -10,26 +10,31 @@ function Navbars() {
   const token = true;
 
   return (
-
-    <Navbar bg="dark" data-bs-theme="dark">
+    <Navbar bg="dark" expand="md" data-bs-theme="dark">
       <Container>
-          <Navbar.Brand href="#home" className="text-white">Pizzería Mamma Mia!</Navbar.Brand>
-          <Nav className="me-auto " >
-            <Nav.Link href="#home" ><Button variant="outline-warning" className="text-white"> 🍕 Home</Button> </Nav.Link>
-            {token ? <Nav.Link href="#Profile" ><Button variant="outline-warning" className="text-white">🔒 Profile</Button></Nav.Link>: <Nav.Link href="#Login" ><Button variant="outline-warning" className="text-white">🔒 Login </Button></Nav.Link>}
-            {token ? <Nav.Link href="#Logout" ><Button variant="outline-warning" className="text-white">🔒 Logout </Button></Nav.Link> : <Nav.Link href="#Register" ><Button variant="outline-warning" className="text-white">🔒 Register</Button></Nav.Link>}
-            
-            
+        <Navbar.Brand href="#home" className="text-white">Pizzería Mamma Mia!</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home"><Button variant="outline-warning" className="text-white">🍕 Home</Button></Nav.Link>
+            {token ? (
+              <Nav.Link href="#Profile"><Button variant="outline-warning" className="text-white">🔒 Profile</Button></Nav.Link>
+            ) : (
+              <Nav.Link href="#Login"><Button variant="outline-warning" className="text-white">🔒 Login</Button></Nav.Link>
+            )}
+            {token ? (
+              <Nav.Link href="#Logout"><Button variant="outline-warning" className="text-white">🔒 Logout</Button></Nav.Link>
+            ) : (
+              <Nav.Link href="#Register"><Button variant="outline-warning" className="text-white">🔒 Register</Button></Nav.Link>
+            )}
           </Nav>
-        </Container>
-          <Form className="d-flex me-4">
-            <Button variant="outline-primary">Total: ${total.toLocaleString()}</Button>
-          </Form>
-
-
+        </Navbar.Collapse>
+        <Form className="d-flex me-4">
+          <Button variant="outline-primary">Total: ${total.toLocaleString()}</Button>
+        </Form>
+      </Container>
     </Navbar>
   );
-
 }
 
 export default Navbars;
